9d5a760b7315866815bbe620f1e0ee59cb2c63e3
Teju-Aws <tejaswinigundluru@gmail.com>
2025-09-15 18:00:20 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,6 +2,6 @@ resource "aws_instance" "jen_server" {
   ami                    = "ami-0b09ffb6d8b58ca91"
   instance_type          = "t2.medium"
   tags = {
-    Name = "jen-serverrrrr"
+    Name = "server"
   }
 }
